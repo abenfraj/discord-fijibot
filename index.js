a6089ffc4389e54af85b3048f5bba973e6a31716
abenfraj <ayoub.benfraj013@gmail.com>
2025-02-27 15:55:36 +0100

    Ping Pong check added

--- index.js
+++ index.js
@@ -146,3 +146,11 @@ async function checkLastMessage() {
 }
 
 client.login(process.env.DISCORD_TOKEN);
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.channel.send("Pong!");
+  }
+});
